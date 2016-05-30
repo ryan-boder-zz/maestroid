24f71090614f5be893f6d56ae9f73c2e37dd4d09
ryan-boder <ryan.boder@gmail.com>
2016-05-29 22:40:07 -0400

    Fixed MainActivityTest deprecated API warning

--- src/androidTest/java/name/ryanboder/maestroid/MainActivityTest.java
+++ src/androidTest/java/name/ryanboder/maestroid/MainActivityTest.java
@@ -15,7 +15,7 @@ import android.test.ActivityInstrumentationTestCase2;
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public MainActivityTest() {
-        super("name.ryanboder.maestroid", MainActivity.class);
+        super(MainActivity.class);
     }
 
 }
