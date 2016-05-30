4b81e809443ce2c25809dfa31328df7c522bd887
ryan-boder <ryan.boder@gmail.com>
2016-05-29 22:29:33 -0400

    Reformatted to scala standard 2 space indentation

--- src/main/scala/name/ryanboder/maestroid/MainActivity.scala
+++ src/main/scala/name/ryanboder/maestroid/MainActivity.scala
@@ -4,12 +4,12 @@ import android.app.Activity
 import android.os.Bundle
 
 class MainActivity extends Activity with TypedFindView {
-    lazy val textview = findView(TR.text)
+  lazy val textview = findView(TR.text)
 
-    /** Called when the activity is first created. */
-    override def onCreate(savedInstanceState: Bundle): Unit = {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
-        textview.setText("Hello world, from Maestroid")
-    }
+  /** Called when the activity is first created. */
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+    textview.setText("Hello world, from Maestroid")
+  }
 }
