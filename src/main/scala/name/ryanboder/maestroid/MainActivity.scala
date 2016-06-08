07dd3bccb6e461a4f07a70c41857c2ebb5e9f10b
ryan-boder <ryan.boder@gmail.com>
2016-06-07 22:43:35 -0400

    Showing accelerometer values on screen

--- src/main/scala/name/ryanboder/maestroid/MainActivity.scala
+++ src/main/scala/name/ryanboder/maestroid/MainActivity.scala
@@ -6,12 +6,16 @@ import org.scaloid.common._
 class MainActivity extends SActivity with TagUtil {
   lazy val accelerometer = new Accelerometer(this)
   lazy val recorder = new AccelerometerRecorder()
-  var recordButton: SButton = null
+  lazy val detector = new GestureDetector()
+
+  private var accelerometerViews: Array[STextView] = null
+  private var recordButton: SButton = null
 
   onCreate {
     info("onCreate")
     contentView = new SVerticalLayout {
       STextView("Welcome to Maestroid")
+      accelerometerViews = Array(STextView(), STextView(), STextView())
       recordButton = SButton("Record Accelerometer", toggleAccelerometerRecording).backgroundColor(Color.DKGRAY)
     }
   }
@@ -25,6 +29,11 @@ class MainActivity extends SActivity with TagUtil {
     info("onResume")
     accelerometer.activate((data: AccelerometerData) => {
       recorder(data)
+      updateAccelerometerViews(data)
+      for (gesture <- detector(data)) {
+        info(gesture.toString)
+        toast(gesture.toString)
+      }
     })
   }
 
@@ -33,6 +42,17 @@ class MainActivity extends SActivity with TagUtil {
     accelerometer.deactivate()
     super.onPause()
   }
+
+  def updateAccelerometerViews(data: AccelerometerData): Unit = {
+    if (data.timestamp - lastLoggedAccelerometerTime > 1000000000) {
+      accelerometerViews(0).setText("X: " + data.x)
+      accelerometerViews(1).setText("Y: " + data.y)
+      accelerometerViews(2).setText("Z: " + data.z)
+      lastLoggedAccelerometerTime = data.timestamp
+    }
+  }
+
+  private var lastLoggedAccelerometerTime = 0L
 
   private def toggleAccelerometerRecording(): Unit = {
     if (recorder.isRecording) {
